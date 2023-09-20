42ee3390d104ca9a3d4c6fbe4eb4abcc6937d219
tim <timdesmet00@gmail.com>
2023-09-20 12:05:30 +0200

    creation des objets

---
+++ script.js
@@ -0,0 +1,171 @@
+const SwordArtOnline = [
+  {
+    title: "Sword Art Online",
+    image: "./image/sao.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["A-1 Pictures", "Tomohiko Ito"],
+    years: "2011 à aujourd'hui",
+    synopsis:
+      "En 2022, l'humanité a réussi à créer une réalité virtuelle. Grâce à un casque, les humains peuvent se plonger entièrement dans le monde virtuel en étant comme déconnectés de la réalité, et Sword Art Online est le premier MMORPG a utiliser ce système. Mais voila que le premier jour de jeu, 10 000 personnes se retrouvent piégées dans cette réalité virtuelle par son créateur : Akihiko Kayaba. Le seul moyen d'en sortir est de finir le jeu. Mais ce ne sera pas facile de sortir de ce monde virtuel puisque si un joueur perd la partie, il meurt également dans la vraie vie. Kirito décide alors de partir à la conquête du jeu en solo, avec pour avantage le fait de faire partie des 1 000 ex-bêta-testeurs.",
+    lien: "https://fr.wikipedia.org/wiki/Sword_Art_Online#Anime",
+  },
+];
+
+const moiSlime = [
+  {
+    title: "Moi, quand je me réincarne en Slime",
+    image: "./image/moiSlim.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["8-Bit, Shinji Ishihara"],
+    years: "2018 à aujourd'hui",
+    synopsis:
+      "Satoru, employé de bureau lambda, se fait assassiner par un criminel en pleine rue. Ainsi, il se retrouve réincarné dans un monde d’heroic-fantasy sous la forme d’un Slime, le monstre le plus faible du bestiaire fantastique. Le voilà équipé de 2 compétences uniques : « Prédateur » qui lui permet de récupérer les aptitudes de ses adversaires, et 'Grand sage' grâce à laquelle il acquiert une compréhension instantanée de son environnement le rendant toujours plus intelligent. Au fil de ses rencontres, Satoru va s’allier à différents guerriers singuliers qui forgeront l’être qu’il décidera de devenir.",
+    lien: "https://fr.wikipedia.org/wiki/Moi,_quand_je_me_r%C3%A9incarne_en_Slime#Adaptation_anim%C3%A9e",
+  },
+];
+
+const logHorizon = [
+  {
+    title: "Log Horizon",
+    image: "./image/LogHorizon.png",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["Satelight, Yasuhito Kikuchi"],
+    years: "2013 à aujourd'hui",
+    synopsis:
+      "Shiroe est amateur de MMORPG. Un jour, alors qu'il joue à Elder Tales, il se retrouve piégé dans le jeu avec 30 000 autres personnes. Désormais, impossible d'utiliser le bouton de log out ou de se faire tuer. Peu à peu, les joueurs perdent la volonté de jouer, comprenant qu'ils y finiront leur vie. Mais Shiroe ne veut pas abandonner. Il débute alors un voyage afin de comprendre d'où vient le problème.",
+    lien: "https://fr.wikipedia.org/wiki/Log_Horizon#Anime",
+  },
+];
+
+const kumaBear = [
+  {
+    title: "Kuma Kuma Kuma Bear",
+    image: "./image/KumaBear.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["EMT Squared, Yuu Nobuta et Hisashii Ishii"],
+    years: "2020 à aujourd'hui",
+    synopsis:
+      "Yuna, une jeune fille de 15 ans vient de commencer à jouer à un VRMMO. Elle décide de s'isoler du monde extérieur pour jouer à ce jeu. En jouant au jeu, elle reçoit une rare tenue d’ours non transférable et surpuissante. Cependant, cette tenue est bien trop voyante pour Yuna et elle ne peut se permettre de la porter, même en jeu. Tout d'un coup Yuna est téléportée dans une forêt inconnue en portant cette fameuse tenue. Yuna va donc commencer une nouvelle aventure dans ce monde avec son costume d'ours surpuissant. ",
+    lien: "https://fr.wikipedia.org/wiki/Kuma_Kuma_Kuma_Bear",
+  },
+];
+
+const nobleNew = [
+  {
+    title: "Noble New World adventures",
+    image: "./image/nobleNew.webp",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["EMT Squared and Magic Bus, Noriyuki Nakamura"],
+    years: "2022 à aujourd'hui",
+    synopsis:
+      "Kazuya Shiina a été tué en protégeant une jeune fille dans un commerce où il se trouvait par hasard, au mauvais moment. En un instant, il s’est retrouvé dans un monde de magie et d'épées ! Réincarné en Cain von Silford, troisième fils d'une famille aristocratique, il s'intéresse rapidement à son nouveau quotidien. Puis, le jour de son cinquième anniversaire, il est baptisé dans une église, selon la tradition. Contre toute attente, les dieux lui accordent plusieurs bénédictions divines, ainsi que des capacités que l'on ne peut décrire que comme extraordinaires ! Sa mission est désormais de faire progresser ce monde en se basant sur les connaissances de sa vie antérieure. Pourtant, lorsqu'il tente de faire les choses correctement, il ne fait que s'attirer des ennuis…",
+    lien: "https://en.wikipedia.org/wiki/Chronicles_of_an_Aristocrat_Reborn_in_Another_World",
+  },
+];
+
+const overlord = [
+  {
+    title: "Overlord",
+    image: "./image/overlord.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["Madhouse, Naoyuki Itō"],
+    years: "2014 à aujourd'hui",
+    synopsis:
+      "2138. 'Yggdrasil', le plus célèbre MMORPG au monde est sur le point de fermer. Momonga, jeune geek, attend patiemment l’arrêt du jeu. Cependant, l’heure de fermeture passée, Momonga ne se déconnecte pas et constate que les personnages non jouables se retrouvent peu à peu dotés d’une conscience… Un nouveau monde est sur le point de faire son apparition.",
+    lien: "https://fr.wikipedia.org/wiki/Overlord_(light_novel)",
+  },
+];
+
+const noGame = [
+  {
+    title: "No Game No Life",
+    image: "./image/noGame.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["Madhouse, Atsuko Ishizuka"],
+    years: "2013 à aujourd'hui",
+    synopsis:
+      "Le désordre est ébranlé par une guerre dévastatrice. Au milieu du chaos, le jeune Riku apparaît et donne un nouvel espoir aux gens. Il veut les conduire vers un avenir doré auquel il croit fermement. Un androïde le soutient dans cette tâche.",
+    lien: "https://fr.wikipedia.org/wiki/No_Game_No_Life",
+  },
+];
+
+const gate = [
+  {
+    title: "Gate",
+    image: "./image/gate.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["A-1 Pictures, Takahiko Kyōgoku"],
+    years: "2014 à 2016",
+    synopsis:
+      "La soif de conquête de L’Empereur d’un Royaume d’heroic-fantasy l’amène à ouvrir une porte dimensionnelle qui donne sur Tokyo, à notre époque. La ville se retrouve envahie par une armée, composée de chevaliers et de bestiaires(monstres). Mais la Force d’auto-défense japonaise réussie à éliminer les envahisseurs et le gouvernement décide d’explorer l’autre monde.",
+    lien: "https://fr.wikipedia.org/wiki/Gate_-_Au-delà_de_la_porte",
+  },
+];
+
+const shieldHero = [
+  {
+    title: "The Rising of the Shield Hero",
+    image: "./image/shieldHero.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["Kinema Citrus, Takao Abo"],
+    years: "2017 à aujourd'hui",
+    synopsis:
+      "Naofumi est projeté dans un monde proche en tout lieu d’un jeu de rôle d’heroic fantasy. Mais alors que d’autres héros ont été dotés d’armes offensives redoutables, Naofumi hérite d’un bouclier aux capacités limitées pour progresser dans ce jeu où le danger peut surgir à chaque instant. Trahi par sa partenaire et vilipendé par la population, le jeune homme ne peut désormais compter que sur lui-même pour survivre dans cet univers hostile… et peut-être sur une jeune fille désœuvrée aux ressources insoupçonnées. ",
+    lien: "https://fr.wikipedia.org/wiki/The_Rising_of_the_Shield_Hero",
+  },
+];
+
+const arifureta = [
+  {
+    title: "Arifureta",
+    image: "./image/arifureta.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["White Fox, Jun Kamiya"],
+    years: "2017 à aujourd'hui",
+    synopsis:
+      "Hajime Nagumo, véritable souffre-douleur, se retrouve transporté avec toute sa classe dans un autre monde. Alors que ses camarades acquièrent des techniques de combat ultra puissantes, Hajime se retrouve doté d’une modeste compétence. Suite à la malveillance d’un camarade, il est laissé pour mort dans un labyrinthe. Hajime, désespéré et le moral en berne, trouve tout de même le moyen de devenir plus fort, et fait une rencontre qui va changer son destin : la vampire Yue.",
+    lien: "https://fr.wikipedia.org/wiki/Arifureta_shokugy%C5%8D_de_sekai_saiky%C5%8D",
+  },
+];
+
+const assassin = [
+  {
+    title:
+      "The World's Finest Assassin Gets Reincarnated in Another World as an Aristocrat",
+    image: "./image/sekai.jpg",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["Silver Link et Studio Palette, Masafumi Tamura"],
+    years: "2021 à aujourd'hui",
+    synopsis:
+      "Quand le plus grand assassin de l'époque est tué, il se réveille devant une Déesse qui lui propose de le réincarner dans un autre monde. Cependant, elle a une condition : il doit assassiner le héros de ce monde.Ayant désormais un objectif précis, il se réincarne en tant que Lugh Tuatha, le dernier-né de la lignée d'assassins de la famille Tuatha Dé.",
+    lien: "https://fr.wikipedia.org/wiki/The_World%27s_Finest_Assassin_Gets_Reincarnated_in_Another_World_as_an_Aristocrat",
+  },
+];
+
+const spider = [
+  {
+    title: "So I'm a Spider, So What?",
+    image: "./image/spider.webp",
+    gender: ["Isekai", "Fantasy", "Adventure"],
+    productor: ["Millepensee, Shin Itagaki"],
+    years: "2018 à aujourd'hui",
+    synopsis:
+      "Je n'étais qu'une lycéenne banale, somnolant en cours, quand soudain... Une brèche s'est ouverte dans la classe, puis tout le monde s'est dématérialisé ! À mon réveil, j'étais réincarnée... en araignée. Un petit insecte de niveau 1, coincé dans un horrible labyrinthe où le danger est partout ! Pour survivre, me voilà condamnée à améliorer mes compétences en affrontant des monstres comme moi et à fuir des aventuriers rebutés par mon apparence...Qu'on me donne la soluce pour réchapper de cet enfer ! Vite, tant qu'il me reste des points de vie !",
+    lien: "https://fr.wikipedia.org/wiki/So_I'm_a_Spider,_So_What%3F",
+  },
+];
+
+const collection = [
+  SwordArtOnline,
+  moiSlime,
+  logHorizon,
+  kumaBear,
+  nobleNew,
+  overlord,
+  noGame,
+  gate,
+  shieldHero,
+  arifureta,
+  assassin,
+  spider,
+];
